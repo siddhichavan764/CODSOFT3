c5010973d8080bc60c9cb93c246a1703b1f21b11
siddhichavan764 <siddhichavan764@gmail.com>
2024-07-20 18:19:44 +0530

    Add files via upload
    tested successfully

---
+++ script.js
@@ -0,0 +1,16 @@
+function addToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    var display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
